53a3c8bf0fa5d38e773bbb6f87485eb8a341d28a
User <User@DESKTOP-0H6MMCT>
2024-04-08 02:29:17 -0500

    Lab RESTful

---
+++ src/main/java/co/edu/unipiloto/arquitecturas/entity/Salario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unipiloto.arquitecturas.entity;
+
+/**
+ *
+ * @author User
+ */
+public class Salario {
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+}
